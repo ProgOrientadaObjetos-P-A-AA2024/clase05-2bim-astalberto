5334e1c53d4287c49f380d4d82b0b36455770faa
astalberto <albertoherrera205@hotmail.com>
2024-07-10 15:43:02 -0500

    Push

--- EjemplosSOLID/EjemplosSOLID/src/paquete06l/Principal.java
+++ EjemplosSOLID/EjemplosSOLID/src/paquete06l/Principal.java
@@ -8,16 +8,18 @@ package paquete06l;
  * @author reroes
  */
 public class Principal {
+
     public static void main(String[] args) {
-        
-        MayorEdad representante = new MayorEdad("José", "Visa");
-        
+        TarjetaCredito visa1 = new TarjetaCredito();
+        visa1.establecerNombre("Visa");
+        visa1.establecerNumero("110011001");
+        MayorEdad representante = new MayorEdad("José", visa1);
+
         MenorEdad menor = new MenorEdad("Felipe", representante);
-        
-        
+
         System.out.printf("Nombre:%s - Tarjeta:%s\n",
-                menor.obtenerNombre(), 
-                menor.obtenerRepresentante().obtenerTarjeta());
-        
+                menor.obtenerNombre(),
+                menor.obtenerRepresentante().obtenerTarjeta().obtenerNumero());
+
     }
 }
---
+++ EjemplosSOLID/EjemplosSOLID/src/paquete06l/TarjetaCredito.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete06l;
+
+/**
+ *
+ * @author utpl
+ */
+public class TarjetaCredito {
+
+    private String nombre;
+    private String numero;
+
+    /*public TarjetaCredito(String tar, String num) {
+        nombre = tar;
+    }*/
+
+    public void establecerNombre(String nom) {
+        nombre = nom;
+    }
+
+    public void establecerNumero(String num) {
+        numero = num;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public String obtenerNumero() {
+        return numero;
+    }
+}
